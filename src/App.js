c7ebd708152825f341405037b27828ee3c2998f0
saifmohammad00 <saifmohammad592001@gmail.com>
2024-07-22 18:12:54 +0530

    Protecting the product page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import NavBar from "./components/Header/NavBar";
 import ContextProvider from "./context/ContextProvider";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./Pages/Home";
 import Store from "./Pages/Store";
 import About from "./Pages/About";
@@ -9,8 +9,11 @@ import Footer from "./components/Footer";
 import ContactUs from "./Pages/ContactUs";
 import ProductDetails from "./Pages/ProductDetails";
 import LoginPage from "./Pages/LoginPage";
+import { useContext } from "react";
+import AuthContext from "./context/auth-context";
 
 const App = () => {
+  const authCtx=useContext(AuthContext);
   return (
     <Router>
       <ContextProvider>
@@ -18,10 +21,11 @@ const App = () => {
         <MainTitle/>
         <Routes>
           <Route path="/" exact element={<Home/>}></Route>
-          <Route path="/store" element={<Store/>}></Route>
           <Route path="/about" element={<About/>}></Route>
           <Route path="/contactus" element={<ContactUs/>}></Route>
           <Route path="/productDetails/:index" element={<ProductDetails/>}></Route>
+          {!authCtx.isLoggedIn && <Route path="/store" element={<Navigate to="/login"/>}></Route>}
+          {authCtx.isLoggedIn && <Route path="/store" element={<Store/>}></Route>}
           <Route path="/login" element={<LoginPage/>}></Route>
         </Routes>
         <Footer/>
